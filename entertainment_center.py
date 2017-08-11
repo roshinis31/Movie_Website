bb1c7161ffd57b6deae336360e30f09de01fb340
roshini31 <roshini.abinaya@gmail.com>
2017-08-11 13:46:11 +0530

    Add files via upload

---
+++ entertainment_center.py
@@ -0,0 +1,9 @@
+import fresh_tomatoes
+import media
+Magadheera = media.Movie("Magadheera","Past life","http://2.bp.blogspot.com/-CzdG8vkFORQ/TjNTLr0GU2I/AAAAAAAAHLE/3EI5FbrGjOc/s1600/Magadheera+730+Days+Poster-1.jpg","https://in.video.search.yahoo.com/video/play;_ylt=A2oKiKW974ZZtkAA.fbmHAx.;_ylu=X3oDMTByZ2N0cmxpBHNlYwNzcgRzbGsDdmlkBHZ0aWQDBGdwb3MDMg--?p=Magadheera+Trailer&vid=e8ad509de78e4a4d7b3e47efbb30e297&turl=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOVP.Wzm7FceC4VXXqDcl0KlCRQEsCb%26pid%3D15.1%26h%3D155%26w%3D300%26c%3D7%26rs%3D1&rurl=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Ds4lakZvzCMY&tit=Magadheera+-+Trailer&c=1&h=155&w=300&l=28&sigr=11bodt10b&sigt=10k7150se&sigi=12rgvpuf9&age=1268537081&fr2=p%3As%2Cv%3Av&fr=mcafee&tt=b")
+Thupakki = media.Movie("Thupaaki","Army man","https://photos.filmibeat.com/ph-big/2012/10/ilayathalapathy-vijay-s-thuppakki-poster_13497552090.jpg","https://www.youtube.com/watch?v=2S0Fk2Dh9Mk")
+Vivegam = media.Movie("Vivegam","Hard working","https://upload.wikimedia.org/wikipedia/en/b/be/Vivegam_poster.jpg","https://www.youtube.com/watch?v=uM7zTAMFRxc")
+Mr_perfect = media.Movie("Mr.perfect","Romance","https://upload.wikimedia.org/wikipedia/en/9/92/Mr._Perfect_poster.jpg","https://www.youtube.com/watch?v=41Lbd7nZJAI")
+Brindhavanam = media.Movie("Brindavanam","Romance","http://1.bp.blogspot.com/-9aS7IfULKf0/UaQUWZJ1e2I/AAAAAAAAA80/mUl0BDq_vdE/s1600/brindavanam.jpg","https://www.youtube.com/watch?v=J9ZhAtJClsE&feature=player_embedded")
+films = [Magadheera,Thupakki,Vivegam,Mr_perfect,Brindhavanam]
+fresh_tomatoes.open_movies_page(films)
